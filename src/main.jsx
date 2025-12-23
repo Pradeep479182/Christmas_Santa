3dbf733d4efe81b7874cd5605cb0e59008b81d42
Dolphin-2002 <dolphin.co.solution@gmail.com>
2025-12-23 10:12:39 +0530

    i fixed the main error that shows inside your project only minor mistakes typo mistakes i marked it inside your repo comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";   // 👈 THIS IS MUST
-import { motion, Motion } from "framer-motion";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  motion
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
